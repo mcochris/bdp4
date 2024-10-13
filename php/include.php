919a0e82b6bb2c0e95fad3dc79b521bab5df0a34
Chris Strawser <46349138+mcochris@users.noreply.github.com>
2024-10-13 17:14:05 -0400

    Defined development status, added error directives

--- php/include.php
+++ php/include.php
@@ -1,7 +1,16 @@
 <?php
 
-define("MUSIC_DIR", "music/Alot of FLAC/Pink Floyd");
+define("PRODUCTION", false);
+define("MUSIC_DIR", "music/Alot of FLAC/");
 
-ini_set("display_errors", 1);
-ini_set("display_startup_errors", 1);
 ini_set("error_reporting", E_ALL);
+ini_set("log_errors", true);
+ini_set("error_log", "bdp4error.log");
+
+if (PRODUCTION) {
+    ini_set("display_errors", false);
+    ini_set("display_startup_errors", false);
+} else {
+    ini_set("display_errors", true);
+    ini_set("display_startup_errors", true);
+}
