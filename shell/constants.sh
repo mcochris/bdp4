09c461a92452a91e10bf77450e3917d9add0e767
Chris Strawser <private>
2024-10-21 19:46:49 -0400

    Initial commit

---
+++ shell/constants.sh
@@ -0,0 +1,6 @@
+readonly IMAGE_FILE_EXTENSIONS='.*\.(jpg|jpeg|webp|bmp|gif|png|ico|jpt|pdf|pgx|tiff)$'
+readonly AUDIO_FILE_EXTENSIONS='.*\.(mp3|flac|wav|ogg|pcm|aiff|aac|wma|alac|wma)$'
+readonly COVER_FILE_SQL_INSERTS="cover_file_inserts.sql"
+readonly DB="bdp4.db"
+readonly LOG=true
+readonly LOG_FILE="find_largest_images.log"
