780995acc99870bf5c7056c46302211520188fc9
Chris Strawser <private>
2024-10-25 11:40:42 -0400

    Moved sanity checks to find_largest_images.sh. Increased scope of scanning. Added MAX_LOG_SIZE.

--- shell/constants.sh
+++ shell/constants.sh
@@ -1,59 +1,14 @@
 #!/usr/bin/env sh
 
-readonly MUSIC_DIR="../../Music/Alot of FLAC/Beethoven/"
+#╔═════════════════════════════════════════════════════════════════════════════
+#║	Constants for the find_largest_images.sh script.
+#╚═════════════════════════════════════════════════════════════════════════════
+
+readonly MUSIC_DIR="../../Music/Alot of FLAC/"
 readonly IMAGE_FILE_EXTENSIONS='.*\.(jpg|jpeg|webp|bmp|gif|png|ico|jpt|pgx|tiff)$'
 readonly AUDIO_FILE_EXTENSIONS='.*\.(mp3|flac|wav|ogg|pcm|aiff|aac|wma|alac|wma)$'
 readonly PID_FILE="pidFile"
 readonly DB="../bdp4.sqlite"
 readonly LOG=true
 readonly LOG_FILE="find_largest_images.log"
-
-[ -f "$DB" ] || {
-	echo Database file "$DB" does not exist >&2
-	exit 10
-}
-
-[ -s "$DB" ] || {
-	echo Database file "$DB" is empty >&2
-	exit 11
-}
-
-[ -d "$MUSIC_DIR" ] || {
-	echo Music directory "$MUSIC_DIR" does not exist >&2
-	exit 111
-}
-
-[ -z "$IMAGE_FILE_EXTENSIONS" ] && {
-	echo Image file extensions are not set >&2
-	exit 12
-}
-
-[ -z "$AUDIO_FILE_EXTENSIONS" ] && {
-	echo Audio file extensions are not set >&2
-	exit 13
-}
-
-[ -z "$PID_FILE" ] && {
-	echo PID file is not set >&2
-	exit 14
-}
-
-[ -f "$PID_FILE" ] && {
-	[ $(find "$PID_FILE" -mmin +60) ] && {
-		echo This program \($(basename "$0")\) has been running for over an hour, something is wrong. Will restart program. >&2
-		rm -f "$PID_FILE"
-	} || {
-		echo This program is already running >&2
-		exit 15
-	}
-}
-
-[ -f $LOG_FILE ] && {
-	[ $(stat -f %z "$LOG_FILE") -gt 10000 ] && {
-		echo Trimming large log file \($LOG_FILE\). >&2
-		tail --lines=100 "$LOG_FILE" > "/tmp/$LOG_FILE"
-		mv "/tmp/$LOG_FILE" "$LOG_FILE"
-	}
-}
-
-return 0
+readonly MAX_LOG_SIZE=1000
